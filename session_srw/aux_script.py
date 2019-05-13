14ae7f3b7d3a221e927d52e515bc0e3bb4fbfe12
Rafael Celestre <rafael.celestre@esrf.fr>
2019-05-13 17:55:09 +0200

    Propagators presentation added; ID16A beamline description.

--- session_srw/aux_script.py
+++ session_srw/aux_script.py
@@ -1,17 +1,5 @@
-try:
-    mesh = in_object_1._SRWData__srw_wavefront.mesh
-    print('Propagated wavefront:')
-    print('Nx = %d, Ny = %d' % (mesh.nx, mesh.ny))
-    print('dx = %.4f um, dy = %.4f um' % ((mesh.xFin - mesh.xStart) * 1E6 / mesh.nx, (mesh.yFin - mesh.yStart) * 1E6 / mesh.ny))
-    print('range x = %.4f um, range y = %.4f um' % ((mesh.xFin - mesh.xStart) * 1E6, (mesh.yFin - mesh.yStart) * 1E6))
-except:
-    pass
-
-try:
-    mesh = in_object_2._SRWData__srw_wavefront.mesh
-    print('Propagated wavefront:')
-    print('Nx = %d, Ny = %d' % (mesh.nx, mesh.ny))
-    print('dx = %.4f um, dy = %.4f um' % ((mesh.xFin - mesh.xStart) * 1E6 / mesh.nx, (mesh.yFin - mesh.yStart) * 1E6 / mesh.ny))
-    print('range x = %.4f um, range y = %.4f um' % ((mesh.xFin - mesh.xStart) * 1E6, (mesh.yFin - mesh.yStart) * 1E6))
-except:
-    pass
+mesh = in_object_1._SRWData__srw_wavefront.mesh
+print('Propagated wavefront:')
+print('Nx = %d, Ny = %d' % (mesh.nx, mesh.ny))
+print('dx = %.4f um, dy = %.4f um' % ((mesh.xFin - mesh.xStart) * 1E6 / mesh.nx, (mesh.yFin - mesh.yStart) * 1E6 / mesh.ny))
+print('range x = %.4f um, range y = %.4f um' % ((mesh.xFin - mesh.xStart) * 1E6, (mesh.yFin - mesh.yStart) * 1E6))
